a889b9c3452c98f40311d74bb8da91230d2440ec
Sanjeet Singh <sk1044ag11@gmail.com>
2021-01-09 11:54:33 +0530

    path correction.

--- index.js
+++ index.js
@@ -10,10 +10,10 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Serve only the static files form the dist directory
-app.use(express.static(__dirname + '/dist/spaceX-launch-programs/'));
+app.use(express.static(__dirname + './dist/spaceX-launch-programs/'));
 
 app.get('/*', function (req, res) {
-  res.sendFile(path.join(__dirname + '/dist/spaceX-launch-programs/index.html'));
+  res.sendFile(path.join(__dirname + './dist/spaceX-launch-programs/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
